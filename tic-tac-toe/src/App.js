5673904cb68ae3985de69970243898f55c99e97b
glendagarcia90 <glenda.garcia0799@gmail.com>
2024-10-25 02:06:01 -0600

    mi primer app con react

---
+++ tic-tac-toe/src/App.js
@@ -0,0 +1,131 @@
+import logo from './logo.svg';
+import { useState } from 'react'; //función especial que puedes llamar desde tu componente para permitirle «recordar» cosas
+import './App.css';
+/*El codigo en App.js crea un componente
+un componente es una pieza de código reutilizable que representa una parte de una interfaz de usuario
+*/
+
+//indica que al componente Square se le puede pasar un objeto llamado value
+function Square({ value, onSquareClick }){
+  return <button className="square" onClick={onSquareClick}>
+    {value}
+  </button>;
+}
+
+/*La palabra export hace que esta funcion sea accesible fuera del archivo
+default le dice a los demas archivos que usan su codigo que es la funcion principal en su archivo*/
+
+/*Return significa despues se vuelve como un valor*/
+function Board({ xIsNext, squares, onPlay }){
+
+  function handleClick(i) {
+
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    const nextSquares = squares.slice();
+    if (xIsNext) {
+      nextSquares[i] = "X";
+    } else {
+      nextSquares[i] = "O";
+    }
+    
+    onPlay(nextSquares);
+  }
+
+  
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = "Ganador: " + winner;
+  } else {
+    status = "Siguiente jugador: " + (xIsNext ? "X" : "O");
+  }
+
+  return (
+    <>
+      <div className="status">{status}</div>
+      <div className="board-row">
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)}/> {/*{() => handleClick(0)} es una función de flecha, que es una forma más corta de definir funciones */}
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)}/>
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)}/>
+      </div>
+
+      <div className="board-row">
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)}/>
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)}/>
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)}/>
+      </div>
+        
+      <div className="board-row">
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)}/>
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)}/>
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)}/>
+      </div> 
+        
+    </>
+    
+  )
+  
+};
+
+export default function Game() {
+
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [currentMove, setCurrentMove] = useState(0);
+  const xIsNext  = currentMove % 2 === 0;
+  const currentSquares = history[currentMove];
+
+  function handlePlay(nextSquares) {
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];//...history como «enumerar todos los elementos en history»
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
+  }
+  function jumpTo(nextMove) {
+    setCurrentMove(nextMove);
+    
+  }
+  const moves = history.map((squares, move) => {
+    let description;
+    if (move > 0) {
+      description = 'Ir al movimiento #' + move;
+    } else {
+      description = 'Ir al inicio del juego';
+    }
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    );
+  });
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay}/>
+      </div>
+      <div className="game-info">
+      <ol>{moves}</ol>
+      </div>
+    </div>
+  );
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
